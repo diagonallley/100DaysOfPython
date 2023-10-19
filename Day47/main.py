8bcc5aadcf00a74480b998a82470ee7024a46f64
userdiagonallley <007shubham007@gmail.com>
2023-10-19 23:46:53 +0530

    keys

--- Day47/main.py
+++ Day47/main.py
@@ -9,11 +9,22 @@ import os
 import re
 import smtplib
 
-def send_mail(price):
-    with smtplib.SMTP("smtp.google.com")
+USER = os.environ.get("GMAIL_USER")
+PWD = os.environ.get("GMAIL_PWD")
 
 
-MIN_PRICE = 350.00
+def send_mail(product, price):
+    print(USER)
+    print(PWD)
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        print("here")
+        connection.starttls()
+        connection.login(user=USER, password=PWD)
+        connection.sendmail(from_addr=USER, to_addrs=USER,
+                            msg=f"Subject:Deal!\n\n{product} is now available at ${price}\n{URL}")
+
+
+MIN_PRICE = 400.00
 
 URL = "https://www.amazon.com/Sony-WH-1000XM5-Canceling-Headphones-Hands-Free/dp/B09XS7JWHH/ref=sr_1_5?crid=ZK8BHRQZQ0FS&keywords=sony+headphones&qid=1697736306&sprefix=sony+hea%2Caps%2C428&sr=8-5"
 # URL = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
@@ -35,8 +46,10 @@ res.raise_for_status()
 soup = BeautifulSoup(res.text, "html.parser")
 # price = soup.select("span.a-price-whole")
 price = soup.select("span#price_inside_buybox")[0].get_text()
+title = soup.select("span#productTitle")[0].get_text().strip()
 pattern = re.compile("[0-9]+.[0-9]+")
 int_price = float(pattern.search(price).group())
 print(int_price)
+print(title)
 if int_price < MIN_PRICE:
-    send_mail(int_price)
+    send_mail(title, int_price)
